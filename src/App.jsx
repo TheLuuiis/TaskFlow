54fd3bc5f32e98f8b191db243d252d56cd862cf7
TheLuis <luisdavidgilcabarcas47@gmail.com>
2026-02-05 12:39:39 -0500

    Desarrollo de rutas y pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './style/globals.css';
-import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-
+import Home from './pages/Home';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header/>
-      
-    </>
+      <main className="app__main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../style/pages/Home.css';
+
+const Home = () => {
+    return (  
+        <div className="container__home">
+            <h1>Page Home</h1>
+        </div>
+    );
+}
+ 
+export default Home;
